053acee3d1c49712a91666a213ba433a84dd373a
sailybay <sailybaybakdaulet@gmail.com>
2026-05-03 13:51:41 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SubscriptionManagerApp());
+}
+
+class SubscriptionManagerApp extends StatelessWidget {
+  const SubscriptionManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
